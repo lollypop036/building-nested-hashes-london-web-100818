247825d66a361af20727684f865c9500d1627c76
Lola Onanuga <slonanuga@gmail.com>
2018-09-29 18:08:10 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -4,6 +4,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Montague", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
+      
       :hero_friends => [
        {:name => "Benvolio", :age => "17", :attitude => "worried"},
         {:name => "Mercuito", :age => "18", :attitude => "hot-headed"}
@@ -18,6 +19,7 @@ def fourth_challenge
         :name => "Steven",
         :age => "30",
         :attitude => "confused"
+      }]
       },
         {
         :name => "Nurse",
